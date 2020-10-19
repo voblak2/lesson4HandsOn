56eb4ec02d830cab08fcf16b51e1607db3324aa9
Vince Oblak <voblak2@gmail.com>
2020-10-18 20:22:56 -0500

    Initial commit hands on for L04

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace lesson4HandsOn
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
